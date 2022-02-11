499ae2e54933bfdf65d0ceccb6b70f335b6fd247
sraut <sraut@LAPTOP-PN4655LM>
2022-02-12 01:57:30 +0530

    BY SHIVAMRAUT

---
+++ GITHUB/src/github/Heloo.java
@@ -0,0 +1,11 @@
+package github;
+
+public class Heloo 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello this is git program");
+	}
+	
+}
---
+++ GITHubb/src/github/Demo.java
@@ -0,0 +1,11 @@
+package github;
+
+public class Demo {
+
+public static void main(String[] args) 
+{
+	System.out.println("Hello");
+}
+	
+
+}
