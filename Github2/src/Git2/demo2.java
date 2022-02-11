f4dc9b2c61ce39be0b8836e1a8e5bd33d9f7f5c6
shivam13raut <99393693+shivam13raut@users.noreply.github.com>
2022-02-12 02:43:38 +0530

    Update demo2.java

--- Github2/src/Git2/demo2.java
+++ Github2/src/Git2/demo2.java
@@ -2,8 +2,13 @@ package Git2;
 
 public class demo2 
 {
-	public void m1() {
+	public void m1() 
+	{
 		System.out.println("M1 method");
+	}
+	public void m2() 
+	{
+		System.out.println("M2 method");
 	}
 	
 	public static void main(String[] args) 
