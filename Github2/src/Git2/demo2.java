d97d00ae7c7bfeb1bc022a839dcd4ef32b99ae9e
sraut <sraut@LAPTOP-PN4655LM>
2022-02-12 02:40:20 +0530

    by SHivam1

--- Github2/src/Git2/demo2.java
+++ Github2/src/Git2/demo2.java
@@ -2,6 +2,10 @@ package Git2;
 
 public class demo2 
 {
+	public void m1() {
+		System.out.println("M1 method");
+	}
+	
 	public static void main(String[] args) 
 	{
 		System.out.println("Hello this is git program");
